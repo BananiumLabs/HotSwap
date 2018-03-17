d6c5f35b7067f1c8bf1b434962bf8fb121102a44
Eric Qian <eric@enumc.com>
2018-03-17 15:16:33 -0700

    Working Database Update

--- microservices/www/src/app/module1/module-1-page.component.ts
+++ microservices/www/src/app/module1/module-1-page.component.ts
@@ -3,6 +3,7 @@ import { RequestOptions } from '@angular/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 import {Router, ActivatedRoute} from '@angular/router';
 import { AngularFireStorage } from 'angularfire2/storage';
+import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 import { MaterializeAction } from 'angular2-materialize';
 import { Listing } from 'app/module1/listing';
 import { MaterializeModule } from 'angular2-materialize';
@@ -19,17 +20,21 @@ import { NgModel } from '@angular/forms';
 export class Module1PageComponent {
 
   entries: Listing[];
-  currListing: Listing = new Listing(); 
+  currListing: Listing = new Listing();
 
 
   modalActions = new EventEmitter<string | MaterializeAction>();
 
-  constructor(private storage: AngularFireStorage, private auth: AngularFireAuth) {
-
+  itemRef: AngularFireObject<any>;
+  item: Observable<any>;
+  imageURL: Observable<string>;
+  constructor(private storage: AngularFireStorage, db: AngularFireDatabase, private auth: AngularFireAuth) {
+    this.itemRef = db.object('item');
+    this.item = this.itemRef.valueChanges();
   }
 
   openModal() {
-    //populate currListing
+    // populate currListing
     this.currListing.authorid = this.auth.auth.currentUser.displayName;
     this.currListing.authoremail = this.auth.auth.currentUser.email;
     this.modalActions.emit({ action: 'modal', params: ['open'] });
@@ -38,8 +43,13 @@ export class Module1PageComponent {
     this.modalActions.emit({ action: 'modal', params: ['close'] });
   }
 
-  add(item:string) {
+  add(item: string) {
     // this.entries.push(item);
+  }
+  saveListing(event) {
+    console.log('Saving Object: ' + this.currListing);
+    this.itemRef.set(this.currListing);
+
   }
   fileChange(event) {
     console.log('Upload Started!');
